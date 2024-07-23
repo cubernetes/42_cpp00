bc8f2a86855f117064b6ca155e67a5f4e40f8099
cubernetes <tischmid@proton.me>
2024-07-23 18:19:07 +0200

    Add cool megaphone.cpp

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,2 @@
+#include<iostream>
+int main(int __,char**_){if (--__)while(*++_)while(**_)std::cout<<(char)toupper(*(*_)++);else std::cout<<"* LOUD AND UNBEARABLE FEEDBACK NOISE *";std::cout<<'\n';}
